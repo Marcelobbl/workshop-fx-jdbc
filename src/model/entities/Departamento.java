0cd7cc31f49f2f86f8d3b0d6753fb1fa14aab52c
Marcelobbl <produtormarcelo@hotmail.com>
2020-04-21 10:14:37 -0300

    DepartamentoList Controller

---
+++ src/model/entities/Departamento.java
@@ -0,0 +1,69 @@
+package model.entities;
+
+import java.io.Serializable;
+
+public class Departamento implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private String nomeDep;
+	
+	public Departamento() {
+		}
+	
+	public Departamento(Integer id, String nomeDep) {
+		this.id = id;
+		this.nomeDep = nomeDep;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNomeDep() {
+		return nomeDep;
+	}
+
+	public void setNomeDep(String nomeDep) {
+		this.nomeDep = nomeDep;
+	}
+
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Departamento other = (Departamento) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Departamento [id=" + id + ", nome=" + nomeDep + "]";
+	}
+	
+		
+}
+
